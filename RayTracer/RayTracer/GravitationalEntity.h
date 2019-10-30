c126d92fcfd71d861705c17a10ed1dea18e38b4d
Unknown <lucasoliveirareis@hotmail.com>
2019-10-30 15:50:15 -0300

    change double to float

--- RayTracer/RayTracer/GravitationalEntity.h
+++ RayTracer/RayTracer/GravitationalEntity.h
@@ -1,21 +1,21 @@
 #pragma once
 
-const double planckConstant = 6.62607015e34; // J*s
-const double c = 299792458; // m/s
-const double G = 6.674e-11; // m^3/(kg*s^2)
+const float planckConstant = 6.62607015e34; // J*s
+const float c = 299792458; // m/s
+const float G = 6.674e-11; // m^3/(kg*s^2)
 
 
 class GravitationalEntity {
 public:
-	double M{ 0 };
-	double schwarzschildRadius{ 0 };
-	double radius;
+	float M{ 0 };
+	float schwarzschildRadius{ 0 };
+	float radius;
 
-	GravitationalEntity(double mass) : M(mass) {
+	GravitationalEntity(float mass) : M(mass) {
 		this->schwarzschildRadius = 2 * G * M / (c * c);
 		this->radius = this->schwarzschildRadius;
 	}
-	GravitationalEntity(double mass, double radius) {
+	GravitationalEntity(float mass, float radius) {
 		this->schwarzschildRadius = 2 * G * M / (c * c);
 		this->radius = radius;
 	}
--- RayTracer/RayTracer/RungeKuttaSolver.h
+++ RayTracer/RayTracer/RungeKuttaSolver.h
@@ -5,16 +5,16 @@
 
 static class RungeKuttaSolver
 {
-	static v3d RungeKutta4Solver(double step, double tFinal, v3d initial, const std::function<v3d(double, v3d)>& f);
+	static v3d RungeKutta4Solver(float step, float tFinal, v3d initial, const std::function<v3d(float, v3d)>& f);
 
 };
 
-v3d RungeKuttaSolver::RungeKutta4Solver(double step, double tFinal, v3d initial, const std::function<v3d(double, v3d)>& f) {
+v3d RungeKuttaSolver::RungeKutta4Solver(float step, float tFinal, v3d initial, const std::function<v3d(float, v3d)>& f) {
 	v3d current = initial;
 	v3d next = initial;
 	v3d k1, k2, k3, k4;
 
-	for (double t = 0; t < tFinal; t += step) {
+	for (float t = 0; t < tFinal; t += step) {
 		current = next;
 		k1 = step * f(t, current);
 		k2 = step * f(t + step / 2, current + k1 / 2);
