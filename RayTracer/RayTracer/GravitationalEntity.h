c6bccaeeddb6a0670c923bb025b40e946ad88138
vitor-k <vitor-kiguchi@hotmail.com>
2019-10-30 16:28:24 -0300

    changes to gravitationalentity

--- RayTracer/RayTracer/GravitationalEntity.h
+++ RayTracer/RayTracer/GravitationalEntity.h
@@ -2,21 +2,18 @@
 #include "Sphere.h"
 
 //const float planckConstant = 6.62607015e34; // J*s   1
-//const float c = 299792458; // m/s                    1
-//const float G = 6.674e-11; // m^3/(kg*s^2)           1
-
+constexpr float c = 1; //299792458; // m/s
+constexpr float G = 1; //6.674e-11; // m^3/(kg*s^2)
 
 class GravitationalEntity : public Sphere {
 public:
 	float M{ 0 };
 	float schwarzschildRadius{ 0 };
 
-	GravitationalEntity(v3d center, float outerRadius, float mass) : Sphere(center, outerRadius) , M(mass) {
-		this->schwarzschildRadius = 2 * M;
+	GravitationalEntity(v3d center, float mass, float radius) : Sphere(center, radius), M(mass){
+		this->schwarzschildRadius = 2 * G * M / (c * c);
 	}
 
-	/*GravitationalEntity(v3d center, float mass) : M(mass) {
-		this->schwarzschildRadius = 2 * M;
-		this->Sphere(center, outerRadius);
-	} TODO */
+	GravitationalEntity(v3d center, float mass) : Sphere(center, 2 * G * M / (c * c)), M(mass), schwarzschildRadius(2 * G * M / (c * c)) {
+	}
 };
