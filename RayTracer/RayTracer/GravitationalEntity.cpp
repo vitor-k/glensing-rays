e6d46a605a49f6e114d77066f28730f1a8072c93
vitor-k <vitor-kiguchi@hotmail.com>
2019-11-11 23:46:12 -0300

    cache previous RK results

--- RayTracer/RayTracer/GravitationalEntity.cpp
+++ RayTracer/RayTracer/GravitationalEntity.cpp
@@ -1,6 +1,6 @@
 #include "GravitationalEntity.h"
 #include "RungeKuttaSolver.h"
-
+#include <cmath>
 
 GravitationalEntity::GravitationalEntity(v3d center, float mass, float radius) : Sphere(center, radius), M(mass),
                                          schwarzschildRadius(2 * G * M / (c * c) ), photonSphere(1.5*schwarzschildRadius){}
@@ -14,14 +14,27 @@ State3d GravitationalEntity::statePonto(const float& t, const State3d& estado) c
 
 bool GravitationalEntity::response(v3d& rayDirection, v3d& intersection, v3d& intersectionNormal, olc::Pixel& pix) {
 	const State3d inicial{ intersection, rayDirection };
+	State3d final;
 
-	State3d final = RungeKuttaSolver::RungeKutta4Solverf(0.001f, 100000, inicial, (*this));
+	if (cachedResults.count({std::round( 400*intersection.x), std::round(400*intersection.y) }) > 0) {
+		State3d final = cachedResults[{std::round(400 * intersection.x), std::round(400 * intersection.y) }];
+		intersection = final.s;
+		rayDirection = final.v;
+		return false;
+	}
+	else if (blackCachedResults.count({ std::round(400 * intersection.x), std::round(400 * intersection.y) }) > 0) {
+		pix = olc::Pixel(0, 0, 0);
+		return true;
+	}
+	final = RungeKuttaSolver::RungeKutta4Solverf(0.001f, 100000, inicial, (*this));
 	//std::cout << (final.s - center).norm2() << std::endl;
 	if ((final.s - center).norm() < photonSphere) { //outerRadius * outerRadius) {
+		blackCachedResults.emplace(std::make_pair<int, int>(std::round(400 * intersection.x), std::round(400 * intersection.y)));
 		pix = olc::Pixel(0, 0, 0);
 		return true;
 	}
 	else {
+		cachedResults.try_emplace(std::make_pair<int,int>(std::round(400 * intersection.x), std::round(400 * intersection.y)), final);
 		intersection = final.s;
 		rayDirection = final.v;
 		return false;
--- RayTracer/RayTracer/GravitationalEntity.h
+++ RayTracer/RayTracer/GravitationalEntity.h
@@ -1,6 +1,10 @@
 #pragma once
 #include "Sphere.h"
 #include "olcPixelGameEngine.h"
+#include "v3d.h"
+#include<map>
+#include<set>
+#include<utility>
 
 //const float planckConstant = 6.62607015e34; // J*s   1
 constexpr float c = 1; //299792458; // m/s
@@ -16,4 +20,6 @@ public:
 	State3d statePonto(const float& t, const State3d& estado) const;
 	bool response(v3d& rayDirection, v3d& intersection, v3d& intersectionNormal, olc::Pixel& pix) override;
 
+	std::map<std::pair<int,int>, State3d> cachedResults;
+	std::set<std::pair<int, int>> blackCachedResults;
 };
